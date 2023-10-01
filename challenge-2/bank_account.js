2683bb5ef6ff42b196f707fc6fbb3259daa641f7
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-10-01 13:23:18 +0700

    fix: export module

--- challenge-2/bank_account.js
+++ challenge-2/bank_account.js
@@ -1,20 +1,42 @@
-var saldo = 0;
-
-const tambahSaldo = () => {
-    const jumlah = window.prompt("Masukkan jumlah saldo yang ingin ditambahkan");
-    saldo += parseInt(jumlah);
+class BankAccount {
+  constructor() {
+    this._balance = 0;
+  }
+  get balance() {
+    return this._balance;
+  }
+  async deposit(amount) {
+    if (amount <= 0) {
+      throw new Error("Jumlah deposit tidak valid!");
+    }
+    console.log("Saldo anda sekarang : " + this._balance);
+    console.log("Menambahkan saldo sebesar " + amount + " ke akun.");
+    console.log("Tunggu 3 detik");
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        this._balance += amount;
+        resolve();
+      }, 3000)
+    );
+    console.log("saldo sekarang: " + this._balance);
+  }
+  async withdraw(amount) {
+    if (amount > this._balance) {
+      throw new Error("Saldo tidak cukup!");
+    } else if (amount <= 0) {
+      throw new Error("Jumlah penarikan tidak valid!");
+    }
+    console.log("Saldo anda sekarang : " + this._balance);
+    console.log("Mengambil saldo sebesar " + amount + " dari akun.");
+    console.log("Tunggu 3 detik");
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        this._balance -= amount;
+        resolve();
+      }, 3000)
+    );
+    console.log("saldo sekarang: " + this._balance);
+  }
 }
 
-const ambilSaldo = () => {
-    const jumlah = window.prompt("Masukkan jumlah saldo yang ingin diambil");
-    saldo -= jumlah;
-}
-
-console.log("Menjalankan fungsi tambahSaldo()");
-tambahSaldo()
-console.log("saldo sekarang: " + saldo);
-
-console.log("Menjalankan fungsi ambilSaldo()");
-ambilSaldo()
-console.log("saldo sekarang: " + saldo);
-
+module.exports = { BankAccount } 
--- challenge-2/banking_system.js
+++ challenge-2/banking_system.js
@@ -1,43 +1,5 @@
-class BankAccount {
-  constructor() {
-    this._balance = 0;
-  }
-  get balance() {
-    return this._balance;
-  }
-  async deposit(amount) {
-    if (amount <= 0) {
-      throw new Error("Jumlah deposit tidak valid!");
-    }
-    console.log("Saldo anda sekarang : " + this._balance);
-    console.log("Menambahkan saldo sebesar " + amount + " ke akun.");
-    console.log("Tunggu 3 detik");
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        this._balance += amount;
-        resolve();
-      }, 3000)
-    );
-    console.log("saldo sekarang: " + this._balance);
-  }
-  async withdraw(amount) {
-    if (amount > this._balance) {
-      throw new Error("Saldo tidak cukup!");
-    } else if (amount <= 0) {
-      throw new Error("Jumlah penarikan tidak valid!");
-    }
-    console.log("Saldo anda sekarang : " + this._balance);
-    console.log("Mengambil saldo sebesar " + amount + " dari akun.");
-    console.log("Tunggu 3 detik");
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        this._balance -= amount;
-        resolve();
-      }, 3000)
-    );
-    console.log("saldo sekarang: " + this._balance);
-  }
-}
+
+const {BankAccount} = require("./bank_account");
 
 const user = new BankAccount();
 
